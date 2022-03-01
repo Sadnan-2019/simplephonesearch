907ca9242ea037b150df3c690952334d3c468bee
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-03-01 23:31:30 +0600

    img resixe

--- phone.js
+++ phone.js
@@ -1,78 +1,71 @@
 // console.log("ll;po vyt");
 
-const url = `https://openapi.programming-hero.com/api/phones?search=samsung`;
-console.log(url)
-fetch(url)
-  .then((res) => res.json())
-       .then((phones) => phone(phones.data));
-//        .then((phones) => console.log(phones.data));
-  
-
-
-
-const phone = phoneDisplay=>{
-
-const PhoneDiv = document.getElementById("about-phone");
-  PhoneDiv.textContent = "";
-  phoneDisplay.forEach((phone) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-                                    
-                                    
-                                    <div class="card h-100 p-4">
-                                        <img src="${phone.image}" class="card-img-top img-fluid w-50 " alt="...">
-                                        <div class="card-body">
-                                          <h5 class="card-title">Brand Name : ${phone.brand}</h5>
-                                          <p class="card-title fw-bolder">Phone Name : ${phone.phone_name}</p>
-                                         </div>
-                                        <div class="card-footer">
-                                        <button class="btn btn-info " onclick="GetphoneId('${phone.slug}')" >Phone Details</button>
-                                        </div>
-                                      </div>
-                                    
-                                    `;
-
-    PhoneDiv.appendChild(div);
-  });
-
-}
+ 
 
 const SearchClick = () => {
-const searchInput = document.getElementById("search-field");
-const searchInputValue = searchInput.value;
+  const searchInput = document.getElementById("search-field");
+  const searchInputValue = searchInput.value;
+  const PhoneDetailsNull = document.getElementById("phone-details");
+  const noFoundError = document.getElementById("no-found");
+  const InputError = document.getElementById("no-input");
+  InputError.innerHTML =""
 
-if (searchInputValue === "" || searchInputValue == null) {
-// console.log("empty>>>")
-const p = document.createElement("p");
-p.innerHTML = alert`please input value`;
-} else {
-//     console.log("done ", searchInputValue);
-LoadPhones(searchInputValue);
-searchInput.value = "";
-}
+
+  if (searchInputValue === "" || searchInputValue == null) {
+    console.log("empty>>>")
+    noFoundError.textContent = "";
+//     const p = document.createElement("p");
+//     p.innerHTML = alert`please input value`;
+    
+//     const div = document.createElement("div");
+
+//     div.innerHTML = `<h3>Please Input Valid Value</h3>
+          
+//           `;
+
+//           InputError.appendChild(div);
+          // InputError.textContent =""
+        
+          const div = document.createElement("div");
+
+    div.innerHTML = `<h3>Please Input Valid Value</h3>
+          
+          `;
+          InputError.appendChild(div);
+
+          // InputError.style.display ="block";
+          // InputError.style.color ="red";
+
+  } else {
+
+    noFoundError.textContent = "";
+    PhoneDetailsNull.textContent = "";
+    //     console.log("done ", searchInputValue);
+    LoadPhones(searchInputValue);
+    searchInput.value = "";
+  }
 };
 
 const LoadPhones = (searchText) => {
-const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-fetch(url)
-.then((res) => res.json())
-//     .then((phones) => console.log(phones.data));
-.then((phones) => DisplayPhone(phones.data));
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    //     .then((phones) => console.log(phones.data));
+    .then((phones) => DisplayPhone(phones.data));
 };
 
 const DisplayPhone = (phoneDisplay) => {
-if (phoneDisplay.length > 0) {
-// console.log("dssdd",phoneDisplay.length);
-const NewphoneDisplay = phoneDisplay.slice(0, 20);
-//     console.log("new", NewphoneDisplay.length);
+  if (phoneDisplay.length > 0) {
+    // console.log("dssdd",phoneDisplay.length);
+    const NewphoneDisplay = phoneDisplay.slice(0, 20);
+    //     console.log("new", NewphoneDisplay.length);
 
-const PhoneDiv = document.getElementById("about-phone");
-PhoneDiv.textContent = "";
-NewphoneDisplay.forEach((phone) => {
-const div = document.createElement("div");
-div.classList.add("col");
-div.innerHTML = `
+    const PhoneDiv = document.getElementById("about-phone");
+    PhoneDiv.textContent = "";
+    NewphoneDisplay.forEach((phone) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
                           
                           
                           <div class="card h-100 p-4">
@@ -88,47 +81,49 @@ div.innerHTML = `
                           
                           `;
 
-PhoneDiv.appendChild(div);
-});
-} else {
+      PhoneDiv.appendChild(div);
+      // noFoundError.appendChild(div).innerHTML="";
+    });
+  } else {
+    const noFoundError = document.getElementById("no-found");
+    const div = document.createElement("div");
 
-          const  noFoundError = document.getElementById("no-found");
-          const div=document.createElement("div");
-         
-          div.innerHTML=`<h3>No Match Result</h3>
+    div.innerHTML = `<h3>No Match Result</h3>
+          <p>Search again</p>
           `;
-          
-          noFoundError.appendChild(div);
-          // noFoundError.appendChild(div).textContent = "";
-         
-          // noFoundError.style.display = "block";
 
-console.log("no data");
-// const p = document.createElement("p");
+    noFoundError.appendChild(div);
+//     PhoneDiv.textContent = "";
+    // div.innerHTML ="";
 
-// p.innerHTML = `please Not found value`;
-}
+    // noFoundError.appendChild(div).textContent = "";
 
-//   console.log(phoneDisplay);
+    // noFoundError.style.display = "block";
+
+    // console.log("no data");
+    // const p = document.createElement("p");
+
+    // p.innerHTML = `please Not found value`;
+  }
+
+  //   console.log(phoneDisplay);
 };
 
-
-
-
 const GetphoneId = (phoneId) => {
-const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
-fetch(url)
-.then((res) => res.json())
-.then((phoneIds) => PhoneDetails(phoneIds.data));
-// .then(phoneIds =>console.log(phoneIds.data))
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((phoneIds) => PhoneDetails(phoneIds.data));
+  // .then(phoneIds =>console.log(phoneIds.data))
 };
 
 const PhoneDetails = (phoneDetail) => {
-const PhoneDetailsDiv = document.getElementById("phone-details");
-const div = document.createElement("div");
-// div.classList.add("col");
-PhoneDetailsDiv.textContent = "";
-div.innerHTML = `
+  const PhoneDetailsDiv = document.getElementById("phone-details");
+
+  const div = document.createElement("div");
+  // div.classList.add("col");
+  PhoneDetailsDiv.textContent = "";
+  div.innerHTML = `
 <div class="card conatiner     d-flex    ">
                   <div class="row">
 
@@ -136,8 +131,8 @@ div.innerHTML = `
                   
                   <div class="col-lg-6  d-flex justify-content-center    ">
                   <img src="${
-                            phoneDetail.image
-                          }" class="  img-fluid my-5  "   alt="..." style="  height:400px"  >
+                    phoneDetail.image
+                  }" class="  img-fluid my-5  "   alt="..." style="  height:400px"  >
                   
                   </div>
 
@@ -171,19 +166,19 @@ div.innerHTML = `
                     phoneDetail.others.Bluetooth
                   }</span><br>
                   <span class="fw-bolder">GPS:
-                  ${"Bluetooth",phoneDetail.others.GPS}
+                  ${("Bluetooth", phoneDetail.others.GPS)}
                   </span><br>
                   <span class="fw-bolder">NFC:
-                  ${"Bluetooth",phoneDetail.others.NFC}
+                  ${("Bluetooth", phoneDetail.others.NFC)}
                   </span><br>
                   <span class="fw-bolder">Radio:
-                  ${"Bluetooth",phoneDetail.others.Radio}
+                  ${("Bluetooth", phoneDetail.others.Radio)}
                   </span><br>
                   <span class="fw-bolder">USB:
-                  ${"Bluetooth",phoneDetail.others.USB}
+                  ${("Bluetooth", phoneDetail.others.USB)}
                   </span><br>
                   <span class="fw-bolder">WLAN:
-                  ${"Bluetooth",phoneDetail.others.WLAN}
+                  ${("Bluetooth", phoneDetail.others.WLAN)}
                   </span>
                    </div>
                   
@@ -196,7 +191,7 @@ div.innerHTML = `
                
 
 `;
-PhoneDetailsDiv.appendChild(div);
+  PhoneDetailsDiv.appendChild(div);
 
-//   console.log(phoneDetail);
+  //   console.log(phoneDetail);
 };
